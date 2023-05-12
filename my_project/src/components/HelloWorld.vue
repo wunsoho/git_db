84b3e66b1be3c6718964741a1ae49210923a440f
wunsoho <wunsoho@naver.com>
2023-05-12 21:03:02 +0900

    hi

--- my_project/src/components/HelloWorld.vue
+++ my_project/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    안녕
+  <div class="hello">
+     안녕 내 이름은 정원이야
   </div>
 </template>
 
@@ -14,6 +14,19 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
