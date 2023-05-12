f7e1bb17c4b8bfbb8b0d59bbb44d3cbdd0f94a8e
이정원 <lee-jeong-won@ijeong-won-ui-MacBookAir.local>
2023-05-12 20:05:28 +0900

    프로젝트 생성

---
+++ my_project/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    안녕
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
